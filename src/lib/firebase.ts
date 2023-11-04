4f3959fb63ecb2c5e2b5ded4e65911de07500057
notTanveer <101289209+notTanveer@users.noreply.github.com>
2023-11-04 12:44:35 +0530

    upload image to  firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,17 +1,35 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: "aideation-yt-5ce79.firebaseapp.com",
-  projectId: "aideation-yt-5ce79",
-  storageBucket: "aideation-yt-5ce79.appspot.com",
-  messagingSenderId: "860950343384",
-  appId: "1:860950343384:web:f9665f200ba1f10b15b1ad"
+  authDomain: "aideation-yt.firebaseapp.com",
+  projectId: "aideation-yt",
+  storageBucket: "aideation-yt.appspot.com",
+  messagingSenderId: "962348384448",
+  appId: "1:962348384448:web:e02758407aba3258d5ad25",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
+
+export async function uploadFileToFirebase(image_url: string, name: string) {
+  try {
+    const response = await fetch(image_url);
+    const buffer = await response.arrayBuffer();
+    const file_name = name.replace(" ", "") + Date.now + ".jpeg";
+    const storageRef = ref(storage, file_name);
+    await uploadBytes(storageRef, buffer, {
+      contentType: "image/jpeg",
+    });
+    const firebase_url = await getDownloadURL(storageRef);
+    return firebase_url;
+  } catch (error) {
+    console.error(error);
+  }
+}
